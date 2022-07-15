947b4579aa93ad67c69aad7a34a1d5523dfc913b
KostyaPC <kbroz82@gmail.com>
2022-07-15 17:57:30 +0300

    Solve system of 2 equations by Newton-Raphson method / Calculating the 1st iteration.

--- src/SEq_NWP_method.java
+++ src/SEq_NWP_method.java
@@ -3,15 +3,26 @@
 class SEq_NWP_method {
 
     static double[][] matrixJ= new double[2][2];
+    static double[] xmatrc = new double[2];
+    static double[] fmatr = new double[2];
 
-    public static void Matris(double x, double y) {
+//    x-vector
+    public static void xmatrix(){
+        xmatrc[0] = 5;
+        xmatrc[1] = 7;
+    }
 
+    public static void fmatrix(){
+        fmatr[0] =  xmatrc[0]*xmatrc[0]-2*xmatrc[0]*xmatrc[1];
+        fmatr[1] = 2*xmatrc[0]+xmatrc[1];
+    }
 
-        matrixJ[0][0] = 2 * x - 2 * y;
-        matrixJ[0][1] = -2 * x;
+//    Jacobian
+    public static void Jacobian(double x, double y) {
+        matrixJ[0][0] = 2 * xmatrc[0] - 2 * xmatrc[1];
+        matrixJ[0][1] = -2 * xmatrc[0];
         matrixJ[1][0] = 2;
         matrixJ[1][1] = 1;
-
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
                 System.out.print(matrixJ[i][j] + "\t");
@@ -19,15 +30,22 @@ class SEq_NWP_method {
             System.out.println();
         }
     }
+
     public static void determinant(){
         double det = matrixJ[0][0]*matrixJ[1][1]-matrixJ[0][1]*matrixJ[1][0];
         System.out.println("Determinant is " +det);
+
+        double res[]=new double[2];
+        for (int k=0;k<2;k++) {
+            res[k] += xmatrc[k] - (1 / det) * fmatr[k];
+            System.out.println(res[k] + "\t");
+            }
     }
 
     public static void main(String[] args) {
-        double x0 = 1;
-        double y0 = 2;
-        Matris(x0, y0);
+        xmatrix();
+        fmatrix();
+        Jacobian(xmatrc[0], xmatrc[1]);
         determinant();
 
     }
