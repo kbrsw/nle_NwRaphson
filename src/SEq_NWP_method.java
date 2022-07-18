626f2fc56ffe81d6bacb3f51d9b4fc7f10998c65
KostyaPC <kbroz82@gmail.com>
2022-07-18 16:52:30 +0300

    Solution of 2 equation by Newton-Raphson method, almost final version

--- src/SEq_NWP_method.java
+++ src/SEq_NWP_method.java
@@ -1,54 +1,49 @@
 // Newton Raphson Method for solving system of equations
 class SEq_NWP_method {
 
-    static double[][] matrixJ = new double[2][2]; //matrix for Jacobian
-    static double[] xmatrc = new double[2]; //matrix for unknowns
-    static double[] fmatr = new double[2];
-
     //    x,y-vector. Set initial guess values
-    public static void xmatrix() {
-        xmatrc[0] = 5;
-        xmatrc[1] = 7;
+
+    static final double EPSILON = 0.0000000000000001;
+
+    static double f1(double x, double y) {return 2 * x * x - 4 * y - 6;}
+
+    static double f2(double x, double y) {return 4 * y * y + x - 5;
     }
 
-    //Set system of equations
-    public static void fmatrix() {
-        fmatr[0] = xmatrc[0] * xmatrc[0] - 2 * xmatrc[0] * xmatrc[1];
-        fmatr[1] = 2 * xmatrc[0] + xmatrc[1];
+    static double df1dx(double x, double y) {
+        return 4*x;
+    }
+
+    static double df1dy(double x, double y) {
+        return -4;
+    }
+
+    static double df2dx(double x, double y) { return 1; }
+
+    static double df2dy(double x, double y) {
+        return 8*y;
     }
 
     //    calculate Jacobian
-    public static void Jacobian(double x, double y) {
-        //here we write partial derivatives of given equations
-        matrixJ[0][0] = 2 * xmatrc[0] - 2 * xmatrc[1];
-        matrixJ[0][1] = -2 * xmatrc[0];
-        matrixJ[1][0] = 2;
-        matrixJ[1][1] = 1;
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                System.out.print(matrixJ[i][j] + "\t");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void determinant() {
-        double det = matrixJ[0][0] * matrixJ[1][1] - matrixJ[0][1] * matrixJ[1][0];
+    static void Jac(double x, double y) {
+        double det = df1dx(x, y) * df2dy(x, y) - df1dy(x, y) * df2dx(x, y);
         System.out.println("Determinant is " + det);
 
-        double[] res = new double[2];
-        for (int k = 0; k < 2; k++) {
-            res[k] += xmatrc[k] - (1 / det) * fmatr[k];
-            System.out.println(res[k] + "\t");
+        while (Math.abs((1 / det) * f1(x, y)) >= EPSILON && Math.abs((1 / det) * f2(x, y)) >= EPSILON)
+        {
+            det = df1dx(x, y) * df2dy(x, y) - df1dy(x, y) * df2dx(x, y);
+            x = x - (1 / det) * f1(x, y);
+            y = y - (1 / det) * f2(x, y);
+            System.out.println("x = " + x + "\t" + "y= " + y);
         }
     }
 
     public static void main(String[] args) {
-        xmatrix();
-        fmatrix();
-        Jacobian(xmatrc[0], xmatrc[1]);
-        determinant();
+        double x0 = 0.3;
+        double y0 = -2;
+        Jac(x0, y0);
 
     }
 }
+
 
