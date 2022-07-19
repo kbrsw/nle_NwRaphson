66ba95ad50be6e15222e12743e2d08053f9853f4
KostyaPC <kbroz82@gmail.com>
2022-07-19 10:59:54 +0300

    Solution of 2 equation by Newton-Raphson method, almost final version

--- src/SEq_NWP_method.java
+++ src/SEq_NWP_method.java
@@ -1,30 +1,34 @@
-// Newton Raphson Method for solving system of equations
+// Newton Raphson Method for solving system of two equations
 class SEq_NWP_method {
 
-    //    x,y-vector. Set initial guess values
+//    Precision
+    static final double EPSILON = 0.00000000000000000001;
 
-    static final double EPSILON = 0.0000000000000001;
-
+//    First function
     static double f1(double x, double y) {return 2 * x * x - 4 * y - 6;}
-
+//  Second function
     static double f2(double x, double y) {return 4 * y * y + x - 5;
     }
 
+// Partial derivative X of 1st function
     static double df1dx(double x, double y) {
         return 4*x;
     }
 
+//  Partial derivative Y of 1st function
     static double df1dy(double x, double y) {
         return -4;
     }
 
+//  Partial derivative X of 2nd function
     static double df2dx(double x, double y) { return 1; }
 
+//  Partial derivative Y of 2nd function
     static double df2dy(double x, double y) {
         return 8*y;
     }
 
-    //    calculate Jacobian
+    //    calculate Jacobian and apply Newton-Raphson method
     static void Jac(double x, double y) {
         double det = df1dx(x, y) * df2dy(x, y) - df1dy(x, y) * df2dx(x, y);
         System.out.println("Determinant is " + det);
@@ -39,11 +43,9 @@ class SEq_NWP_method {
     }
 
     public static void main(String[] args) {
-        double x0 = 0.3;
+        double x0 = 1.3;
         double y0 = -2;
         Jac(x0, y0);
 
     }
 }
-
-
